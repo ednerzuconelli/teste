5f2c4fe239cbddf54c751f2126385120ed65eb23
zeus_83 <ednerzuconelli@gmail.com>
2024-08-21 17:53:12 -0300

    ....

--- libs/atualizar.bat
+++ libs/atualizar.bat
@@ -3,7 +3,7 @@ git fetch origin
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse @{u})
 if [ $LOCAL != $REMOTE ]; then
-  git pull origin main
-  sudo systemctl restart server.service
-  echo "Projeto atualizado e serviço reiniciado em $(date)" > /home/kuro/update_log.txt
+   git pull origin main
+   sudo systemctl restart server.service
+   echo "Projeto atualizado e serviço reiniciado em $(date)" > /home/kuro/update_log.txt
 fi
